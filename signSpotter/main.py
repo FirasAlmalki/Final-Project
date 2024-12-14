f549321eae926b964bafb53180857bf13e73f79b
Firasalmalki <firasaalmalki@gmail.com>
2024-12-14 18:45:18 +0300

    Add new 'runs' folder with training results

--- signSpotter/main.py
+++ signSpotter/main.py
@@ -5,7 +5,7 @@ def train_model():
     model = YOLO("yolov10x.yaml")  # build a new model from scratch
 
     # Use the model
-    model.train(data="signboard.yaml", epochs=500, device='cuda')
+    model.train(data="signboard.yaml", epochs=500, device='cpu')
 
 if __name__ == '__main__':  # Corrected the name check
     train_model()  # Corrected the function call
