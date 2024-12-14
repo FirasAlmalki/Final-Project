960f4e7556a402b777df44908cdc1cf1b8cf4d9d
Firasalmalki <firasaalmalki@gmail.com>
2024-12-14 17:44:32 +0300

    Initial commit

---
+++ signSpotter/main.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+def train_model():
+    # Load a model
+    model = YOLO("yolov10x.yaml")  # build a new model from scratch
+
+    # Use the model
+    model.train(data="signboard.yaml", epochs=500, device='cuda')
+
+if __name__ == '__main__':  # Corrected the name check
+    train_model()  # Corrected the function call
